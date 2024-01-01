681ff270a63c0f0d146f6b95ab114c86b5274d8c
faresfares15 <lf_aoufar@esi.dz>
2024-01-01 16:59:13 +0100

    started the modal

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -9,7 +9,8 @@ import {
     ModalContent,
     ModalFooter,
     ModalHeader,
-    ScrollShadow, useDisclosure
+    ScrollShadow,
+    useDisclosure
 } from "@nextui-org/react";
 import {Article} from "@/types"
 import Image from "next/image";
@@ -33,7 +34,7 @@ export default function Feed({articles, searchValue}: FeedProps) {
                                 key={index}
                                 className="card-hover-effect transition"
                                 shadow="md"
-                                onPress={ () =>{
+                                onPress={() => {
                                     setCardIndex(index)
                                     onOpen()
                                 }}
@@ -74,44 +75,51 @@ export default function Feed({articles, searchValue}: FeedProps) {
                     ))}
                 </div>
             </ScrollShadow>
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange} backdrop="blur">
-                <ModalContent>
-                    {(onClose) => (
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange} backdrop="blur" size="4xl">
+                <ModalContent className="max-h-[90vh] h-[90vh] overflow-scroll p-4" >
                         <>
-                            <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                            <ModalBody>
-                                <p>
-                                    {cardIndex}
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Nullam pulvinar risus non risus hendrerit venenatis.
-                                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Nullam pulvinar risus non risus hendrerit venenatis.
-                                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Magna exercitation reprehenderit magna aute tempor cupidatat
-                                    consequat elit
-                                    dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum
-                                    quis.
-                                    Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor
-                                    eiusmod.
-                                    Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur
-                                    proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                                </p>
+                            <ModalHeader className="flex justify-between items-center">
+                                <h1 className="text-4xl font-bold ">
+                                    {articles[cardIndex].title}
+                                </h1>
+                                <Button isIconOnly className="bg-transparent">
+                                    <Image src="/assets/icons/star.svg" alt="Star" width={35} height={35}/>
+                                </Button>
+                            </ModalHeader>
+                            <ModalBody >
+                                <div className="flex flex-col">
+                                    <div className="flex gap-4 w-full flex-wrap">
+                                        {articles[cardIndex].keywords.map((keyword, index) => (
+                                            <Chip key={index} className="shrink-0 text-[#17C964] bg-[#E8FAF0] text-xl"
+                                                  variant="flat" size="md">{keyword}</Chip>
+                                        ))}
+                                    </div>
+                                    <div className="my-2 text-gray-700">
+                                        <h2 className="text-2xl font-bold">Description</h2>
+                                        <p className="text-lg">{articles[cardIndex].summary}</p>
+                                    {/*    TODO: change to summary, I'm using the content just for testing*/}
+                                    </div>
+                                    <div className="my-2 text-gray-700">
+                                        <h2 className="text-2xl font-bold">Authors</h2>
+                                        <p className="text-lg">{articles[cardIndex].authors.map(author => `${author.first_name} ${author.last_name}`).join(', ')}</p>
+                                    </div>
+                                    <div className="my-2 text-gray-700">
+                                        <h2 className="text-2xl font-bold">Institutions</h2>
+                                        <p className="text-lg">{articles[cardIndex].institutions.join(', ')}</p>
+                                    </div>
+                                    <div className="my-2 text-gray-700">
+                                        <h2 className="text-2xl font-bold">References</h2>
+                                        <ul className="list-disc pl-5">
+                                            {articles[cardIndex].references.map((reference, index) => (
+                                                <li key={index} className="text-lg my-1">
+                                                    {reference}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
                             </ModalBody>
-                            <ModalFooter>
-                                <Button color="danger" variant="light" onPress={onClose}>
-                                    Close
-                                </Button>
-                                <Button color="primary" onPress={onClose}>
-                                    Action
-                                </Button>
-                            </ModalFooter>
                         </>
-                    )}
                 </ModalContent>
             </Modal>
         </div>
