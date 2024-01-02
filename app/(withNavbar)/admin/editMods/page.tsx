cbe78c1cbe87f017a545ad401e48dac825c224e4
ilyes <armilyes@gmail.com>
2024-01-02 18:14:25 +0100

    finishing CRUD on mods for admin

--- app/(withNavbar)/admin/editMods/page.tsx
+++ app/(withNavbar)/admin/editMods/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Tooltip, Popover , PopoverContent ,PopoverTrigger} from "@nextui-org/react";
+import { Button, Modal, ModalContent, ModalHeader, ModalBody, Input, ModalFooter, useDisclosure} from "@nextui-org/react";
 import {EditIcon} from "@/public/assets/icons/editIcon";
 import {DeleteIcon} from "@/public/assets/icons/DeleteIcon";
 import {EyeIcon} from "@/public/assets/icons/EyeIcon";
@@ -8,16 +8,14 @@ import { useCallback, useEffect, useState } from "react";
 import { toast } from "sonner";
 import TableUsers from "@/components/TableUsers/TableUsers";
 
-type ColumnType = typeof columns[0];
-
-//TODO : define realtype of user we used in db
-type User = typeof users[0];
-
 
 
 export default function App() {
-    return(<div>
-        
+    
+    return(<div className=" w-2/3 m-auto">
+        <p className='purple_gradient font-black text-3xl my-4'> Moderators </p>
+        <p className="font-semibold">you can edit moderators from here </p>
         <TableUsers columns={columns}/>
+        
     </div>)
 }
--- components/TableUsers/TableUsers.tsx
+++ components/TableUsers/TableUsers.tsx
@@ -1,139 +1,198 @@
 "use client"
-
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Tooltip, Popover , PopoverContent ,PopoverTrigger, Button, Input, Modal, useDisclosure, ModalFooter, ModalBody, ModalContent, ModalHeader, user} from "@nextui-org/react";
-import {EditIcon} from "@/public/assets/icons/editIcon";
-import {DeleteIcon} from "@/public/assets/icons/DeleteIcon";
-import {EyeIcon} from "@/public/assets/icons/EyeIcon";
-import { useCallback, useEffect, useState } from "react";
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+    Tooltip,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+    Button,
+    Input,
+    Modal,
+    useDisclosure,
+    ModalFooter,
+    ModalBody,
+    ModalContent,
+    ModalHeader,
+} from "@nextui-org/react";
+    import { EditIcon } from "@/public/assets/icons/editIcon";
+    import { DeleteIcon } from "@/public/assets/icons/DeleteIcon";
+    import { EyeIcon } from "@/public/assets/icons/EyeIcon";
+import { use, useCallback, useEffect, useState } from "react";
 import { toast } from "sonner";
 import { User } from "@/types";
+import Image from "next/image";
+import plus from "@/public/assets/icons/plus.svg";
 
 type ColumnType = {
     name: string;
     uid: string;
 };
 
-export default function TableUsers({columns}: {columns: ColumnType[]}) {
+    export default function TableUsers({ columns }: { columns: ColumnType[] }) {
     const [userDetails, setUserDetails] = useState<Record<string, User>>({});
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
     const [users, setUsers] = useState<User[]>([]);
     const [selectedUser, setSelectedUser] = useState<User>(users[0]);
     const [dataFetched, setDataFetched] = useState(false);
+    const { isOpen: isOpen2, onOpen: onOpen2, onOpenChange: onOpenChange2 } =
+        useDisclosure();
+    const [newUser, setNewUser] = useState<User>({
+        id: "",
+        first_name: "",
+        last_name: "",
+        username: "",
+        email: "",
+        role: "Moderator",
+    });
+
+    const handleInputChange = (key: string, value: string) => {
+        setNewUser((prevUser) => ({ ...prevUser, [key]: value }));
+    };
+
+    const handleFirstNameChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+        handleInputChange("first_name", e.target.value),
+        []
+    );
+
+    const handleLastNameChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+        handleInputChange("last_name", e.target.value),
+        []
+    );
+
+    const handleEmailChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+        handleInputChange("email", e.target.value),
+        []
+    );
+
+    const handleUsernameChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+        handleInputChange("username", e.target.value),
+        []
+    );
+
+    const handlePasswordChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+        handleInputChange("password", e.target.value),
+        []
+    );
+
+    const handleSubmitNewUser = useCallback(() => {
+        setUsers((prevUsers) => [...prevUsers, newUser]);
+        toast.success("User added successfully");
+    }, [newUser]);
+
+    const fetchData = async () => {
+      // TODO: Make a request to fetch user data from API
+      // const response = await fetch("URL");
+      // const data = await response.json();
+      // setUsers(data);
+        setUsers([
+        {
+            id: "1",
+            first_name: "Tony",
+            last_name: "Reichert",
+            role: "Moderator",
+            username: "tony.reichert",
+            email: "tony.reichert@example.com",
+        },
+        {
+            id: '2',
+            first_name: "Zoey",
+            last_name: "Lang",
+            username: "zoey.lang",
+            role: "Technical Lead",
+            email: "zoey.lang@example.com",
+        },
+        {
+            id: '3',
+            first_name: "Jane",
+            last_name: "Fisher",
+            username: "jane.fisher",
+            role: "Senior Developer",
+            email: "jane.fisher@example.com",
+        },
+        {
+            id: '4',
+            first_name: "William",
+            last_name: "Howard",
+            username: "william.howard",
+            role: "Community Manager",
+            email: "william.howard@example.com",
+        },
+        {
+            id: '5',
+            first_name: "Kristen",
+            last_name: "Copper",
+            username: "kristen.copper",
+            role: "Sales Manager",
+            email: "kristen.cooper@example.com",
+        },
+        ]);
+        setDataFetched(true);
+    };
 
     useEffect(() => {
-        //TODO : decomment this code after
-        // const getUsers = async () => {
-        //     const response = await fetch("URL");//TODO : add the url of the api
-        //     if (!response.ok) {
-        //         toast.error("Something went wrong");
-        //         return;
-        //     }
-        //     const data = await response.json();
-        //     if (data.error) {
-        //         toast.error(data.error);
-        //         return;
-        //     }
-        //     setUsers(data);  
-        //     setDataFetched(true);
-        // };
-        // getUsers();
-        setUsers([
-            {
-                id: '1',
-                first_name: "Tony",
-                last_name: "Reichert",
-                role: "Moderator",
-                username: "tony.reichert",
-                email: "tony.reichert@example.com",
-            },
-            {
-                id: '2',
-                first_name: "Zoey",
-                last_name: "Lang",
-                username: "zoey.lang",
-                role: "Technical Lead",
-                email: "zoey.lang@example.com",
-            },
-            {
-                id: '3',
-                first_name: "Jane",
-                last_name: "Fisher",
-                username: "jane.fisher",
-                role: "Senior Developer",
-                email: "jane.fisher@example.com",
-            },
-            {
-                id: '4',
-                first_name: "William",
-                last_name: "Howard",
-                username: "william.howard",
-                role: "Community Manager",
-                email: "william.howard@example.com",
-            },
-            {
-                id: '5',
-                first_name: "Kristen",
-                last_name: "Copper",
-                username: "kristen.copper",
-                role: "Sales Manager",
-                email: "kristen.cooper@example.com",
-            },
-            ]);
-            setDataFetched(true);
+        fetchData();
     }, []);
 
     useEffect(() => {
-        const initialUserDetails = users.reduce((acc, user) => {
-            acc[user.id] = user;
-            return acc;
-        }, {} as Record<string, User>);
+        const initialUserDetails = users.reduce(
+        (acc, user) => ({ ...acc, [user.id]: user }),
+        {} as Record<string, User>
+        );
         setUserDetails(initialUserDetails);
     }, [users]);
 
     const handleSubmit = async (userId: string) => {
-        
         const user = userDetails[userId];
-        setUsers((users) => {
-            const index = users.findIndex((user) => user.id === userId);
-            const newUsers = [...users];
-            newUsers[index] = user;
-            return newUsers;
+        setUsers((prevUsers) => {
+        const index = prevUsers.findIndex((user) => user.id === userId);
+        const newUsers = [...prevUsers];
+        newUsers[index] = user;
+        return newUsers;
         });
-        console.log(user)
-        //TODO : Make the API request to update the user's details and return a toast
+      // TODO: Make the API request to update the user's details and return a toast
+        console.log(user);
     };
 
     const handleDelete = async (userId: string) => {
-        setUsers((users) => users.filter((user) => user.id !== userId));
-        //TODO : Make the API request to delete the user and return a toast
+        setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+      // TODO: Make the API request to delete the user and return a toast
     };
-    
-    const renderCell = useCallback((user: User, columnKey: ColumnType) => {
-    switch (columnKey.uid) {
-        case "name":
-        return (
-            <p className=" font-semibold">
+
+    const renderCell = useCallback(
+        (user: User, columnKey: ColumnType) => {
+        switch (columnKey.uid) {
+            case "name":
+            return (
+                <p className="font-semibold">
                 {user.first_name} {user.last_name}
-            </p>
-            
-        );
-        case "email":
-        return (
-            <div className="flex flex-col">
-            <p className="text-gray-500 text-sm capitalize">{user.email}</p>
-            </div>
-        );
-        case "actions":
-        return (
-            <div className="relative flex items-center gap-2">
-            
-            <Popover placement="bottom">
-                <PopoverTrigger>                
+                </p>
+            );
+            case "email":
+            return (
+                <div className="flex flex-col">
+                <p className="text-gray-500 text-sm capitalize">{user.email}</p>
+                </div>
+            );
+            case "actions":
+            return (
+                <div className="relative flex items-center gap-2">
+                <Popover placement="bottom">
+                    <PopoverTrigger>
                     <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <EyeIcon/>
+                        <EyeIcon />
                     </span>
-                </PopoverTrigger>
-                <PopoverContent className="py-5 px-6 items-start">
+                    </PopoverTrigger>
+                    <PopoverContent className="py-5 px-6 items-start">
                     <p className=" font-bold text-gray-500">
                         First Name : <span className=" font-normal text-black"> {user.first_name} </span>
                     </p>
@@ -149,67 +208,76 @@ export default function TableUsers({columns}: {columns: ColumnType[]}) {
                     <p className=" font-bold text-gray-500">
                         Role : <span className=" font-normal text-black"> {user.role} </span>
                     </p>
-                    
-                </PopoverContent>
-            </Popover>
-            
-            <span onClick={()=>{
+                    </PopoverContent>
+                </Popover>
+
+                <span
+                    onClick={() => {
                     setSelectedUser(user);
                     onOpen();
-                }} className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                <EditIcon/>
-            </span>
-            <Popover placement="bottom">
-                <PopoverTrigger>                
+                    }}
+                    className="text-lg text-default-400 cursor-pointer active:opacity-50"
+                >
+                    <EditIcon />
+                </span>
+                <Popover placement="bottom">
+                    <PopoverTrigger>
                     <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                    <DeleteIcon/>
+                        <DeleteIcon />
                     </span>
-                </PopoverTrigger>
-                <PopoverContent className="p-5">
-                    {/*render a button to say are you sure and one for cancel */}
+                    </PopoverTrigger>
+                    <PopoverContent className="p-5">
                     <p className=" font-bold text-gray-500">
                         Are you sure you want to delete this user ?
                     </p>
                     <Button color="danger" variant="light" onClick={()=>{handleDelete(user.id)}}  >
                         Delete
                     </Button>
-                </PopoverContent>
-            </Popover>
-            </div>
-        );
-        default:
-        return "";
-    }
-    }, []);
+                    </PopoverContent>
+                </Popover>
+                </div>
+            );
+            default:
+            return "";
+        }
+        },
+        []
+    );
 
     const renderHeader = (column: ColumnType) => (
-    <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+        <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
         {column.name}
-    </TableColumn>
+        </TableColumn>
     );
 
     const renderRow = (item: User) => (
         <TableRow key={item.id}>
-            {columns.map((column) => (
+        {columns.map((column) => (
             <TableCell key={column.uid}>{renderCell(item, column)}</TableCell>
-            ))}
+        ))}
         </TableRow>
     );
-
+            
     return (
         <>
-    {dataFetched && users.length > 0 && (
-    <Table className=" w-2/3 mx-auto my-8">
-        <TableHeader columns={columns}>
-            {columns.map(renderHeader)}
-        </TableHeader>
-        <TableBody items={users}>
-            {users.map(renderRow)}
-        </TableBody>
-    </Table>
-    )}
+        {dataFetched && users.length > 0 && (
+            <>
+            <Button className="mt-4 font-bold" size="sm" color="primary" onClick={onOpen2}>
+                <Image src={plus} width={15} height={15} alt="plus" className="mr-2" />
+                Add Moderator
+            </Button>
+            <Table className="w-full mx-auto my-4">
+                <TableHeader columns={columns}>
+                {columns.map(renderHeader)}
+                </TableHeader>
+                <TableBody items={users}>
+                {users.map(renderRow)}
+                </TableBody>
+            </Table>
+            </>
+        )}
 
-    {dataFetched && users.length === 0 && (
+        {dataFetched && users.length === 0 && (
         <div className="flex flex-col items-center justify-center h-96">
             <p className="text-2xl font-bold text-gray-500">No Moderators found</p>
         </div>)
@@ -244,6 +312,38 @@ export default function TableUsers({columns}: {columns: ColumnType[]}) {
                             </Button>
                             <Button color="primary" onPress={() => {
                                 selectedUser && handleSubmit(selectedUser.id);
+                                closeModal();
+                            }}>
+                                Save
+                            </Button>
+                        </ModalFooter>
+                    </>
+                    )}
+                </ModalContent>
+            </Modal>
+
+
+            {/*new user modal */}
+            <Modal isOpen={isOpen2} onOpenChange={onOpenChange2}>
+                <ModalContent>
+                    {(closeModal) => (
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">Edit User</ModalHeader>
+                        <ModalBody>
+                            <form>
+                                <Input className=" my-1" label="first name" type="text" onChange={handleFirstNameChange}  />
+                                <Input className=" my-1" label="last name" type="text" onChange={handleLastNameChange}  />
+                                <Input className=" my-1" label="email" type="email" onChange={handleEmailChange}  />
+                                <Input className=" my-1" label="username" type="text" onChange={handleUsernameChange}  />
+                                <Input className=" my-1" label="password" type="password" onChange={handlePasswordChange}  />
+                            </form>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="danger" variant="light" onPress={closeModal}>
+                                Cancel
+                            </Button>
+                            <Button color="primary" onClick={() => {
+                                handleSubmitNewUser();
                                 closeModal();
                             }}>
                                 Save
